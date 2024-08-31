754a20ec16274b897003e5f1b937c0accc4029a2
parknayun <nayun3454@gmail.com>
2024-08-31 16:04:18 +0900

    player
    delay

--- Assets/Scripts/Camera/ThirdCam.cs
+++ Assets/Scripts/Camera/ThirdCam.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class ThirdCam : FollowCam
 {
+    [SerializeField] private float offsetX;
+    [SerializeField] private float offsetY;
     [SerializeField] private float offsetZ;
     [SerializeField] private float DelayTime;
 
     override protected void FollowCamera()
     {
-        Vector3 FixedPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z + offsetZ);
-        transform.position = Vector3.Lerp(transform.position, FixedPos, DelayTime);
+        Vector3 FixedPos = new Vector3(target.transform.position.x + offsetX,
+            transform.position.y + offsetY, target.transform.position.z + offsetZ);
+        transform.position = Vector3.Lerp(transform.position, FixedPos, DelayTime * Time.deltaTime);
     }
 }
