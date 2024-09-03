67bdaba8956fbdcf0588d17037dfc7c376586140
parknayun <136887700+parknayun@users.noreply.github.com>
2024-09-04 03:24:00 +0900

    0904
    0323

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController cc;
-    private Camera cam;
+    private bool isGround = true;
+    private bool isJump = false;
 
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    
+    private float gravity;
 
     private void Start()
     {
@@ -18,6 +22,8 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         Move();
+        CheckGrounded();
+        TryJump();
     }
 
     private void Move()
@@ -27,14 +33,34 @@ public class PlayerMovement : MonoBehaviour
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
             
+            Vector3 dir = new Vector3(h, 0, v);
+            gravity += Physics.gravity.y * Time.deltaTime;
+            dir.y = gravity;
+
             if (h != 0 || v != 0)
             {
-                Vector3 dir = new Vector3(h, 0, v).normalized;
                 float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
-            
                 transform.rotation = Quaternion.Euler(0F, angle, 0F);
-                cc.Move(dir * moveSpeed * Time.deltaTime);
             }
+            cc.Move(dir * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void CheckGrounded()
+    {
+        if (cc.isGrounded)
+        {
+            isGround = true;
+            isJump = false;
+        }
+        else isGround = false;
+    }
+
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround && !isJump)
+        {
+            gravity = jumpForce;
         }
     }
 }
