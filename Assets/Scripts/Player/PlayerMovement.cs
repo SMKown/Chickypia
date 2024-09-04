2a1381065d76fdf7db70d775a24a7610741bd99b
parknayun <136887700+parknayun@users.noreply.github.com>
2024-09-05 01:37:43 +0900

    0905
    playeranim

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController cc;
+    private Animator animator;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
     
     private float gravity;
-    private bool isJump = false;
+    private bool tryJump = false;
+    private bool jumpQueued = false;
 
     private void Start()
     {
         cc = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -39,8 +42,11 @@ public class PlayerMovement : MonoBehaviour
             {
                 float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.Euler(0F, angle, 0F);
+                cc.Move(dir * moveSpeed * Time.deltaTime);
+                animator.SetBool("isWalk", true);
             }
-            cc.Move(dir * moveSpeed * Time.deltaTime);
+            else
+                animator.SetBool("isWalk", false);
         }
     }
 
@@ -49,17 +55,46 @@ public class PlayerMovement : MonoBehaviour
         if (cc.isGrounded)
         {
             PlayerInfo.Instance.isGround = true;
-            isJump = false;
+            tryJump = false;
+            gravity = 0;
+            animator.ResetTrigger("Jump");
+
+            if (jumpQueued)
+            {
+                jumpQueued = false;
+                tryJump = true;
+            }
         }
-        else PlayerInfo.Instance.isGround = false;
+        else
+        {
+            PlayerInfo.Instance.isGround = false;
+        }
     }
 
     private void TryJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (PlayerInfo.Instance.isGround && !PlayerInfo.Instance.shouldAttack && !isJump)
+            if (PlayerInfo.Instance.isGround && !PlayerInfo.Instance.shouldAttack && !tryJump)
+            {
+                tryJump = true;
                 gravity = jumpForce;
+                animator.SetTrigger("Jump");
+            }
+            else if (!PlayerInfo.Instance.isGround)
+            {
+                jumpQueued = true;
+            }
+        }
+
+        if (tryJump)
+        {
+            if (PlayerInfo.Instance.isGround)
+            {
+                cc.Move(Vector3.up * jumpForce * Time.deltaTime);
+                tryJump = false;
+                gravity = 0;
+            }
         }
     }
 }
