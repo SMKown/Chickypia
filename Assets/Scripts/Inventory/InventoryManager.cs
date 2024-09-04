9923101948ca672474663da8cc07ae2ec8c7aa49
HoHyeonMyung <>
2024-09-04 14:59:10 +0900

    inventory..

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,74 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public GameObject MainInvetory;
+    public GameObject HotbarInventory;
     public int maxItem = 10;
     public InventorySlot[] inventoryslots;
     public GameObject inventoryItemPrefab;
-    public GameObject Hotbar;
 
-    int selectedSlot = -1;
+    private bool isInventoryOpen = false;
+    private int selectedSlot = -1;
+    private int InvenSelectedSlot = -1;
+    private int HotbarSelectedSlot = -1;
 
     private void Start()
     {
-        ChangeSelectedSlot(0);
+        ChangeSelectedSlot(33);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        OnInventory();
+
+        if (isInventoryOpen)
         {
-            if(Hotbar == true)
-                ChangeSelectedSlot(0);  
-            else
-                ChangeSelectedSlot(33);
+            for (int i = 0; i < 6; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    InvenSelectedSlot = i;
+                    ChangeSelectedSlot(InvenSelectedSlot);
+                    break;
+                }
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        else
         {
-            if (Hotbar == true)
-                ChangeSelectedSlot(1);
-            else
-                ChangeSelectedSlot(34);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ChangeSelectedSlot(2);
-            ChangeSelectedSlot(35);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            ChangeSelectedSlot(3);
-            ChangeSelectedSlot(36);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            ChangeSelectedSlot(4);
-            ChangeSelectedSlot(37);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            ChangeSelectedSlot(5);
-            ChangeSelectedSlot(38);
+            for (int i = 0; i < 6; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    HotbarSelectedSlot = i + 33;
+                    ChangeSelectedSlot(HotbarSelectedSlot);
+                    break;
+                }
+            }
         }
     }
 
-    void ChangeSelectedSlot(int newValue)
+    public void OnInventory()
     {
-        if (selectedSlot >= 0)
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            isInventoryOpen = !isInventoryOpen;
+            MainInvetory.SetActive(isInventoryOpen);
+            HotbarInventory.SetActive(!isInventoryOpen);
+
+            if (isInventoryOpen)
+            {
+                InvenSelectedSlot = HotbarSelectedSlot - 33;
+                ChangeSelectedSlot(InvenSelectedSlot);
+            }
+            else
+            {
+                HotbarSelectedSlot = InvenSelectedSlot + 33;
+                ChangeSelectedSlot(HotbarSelectedSlot);
+            }
+        }
+    }
+
+    private void ChangeSelectedSlot(int newValue)
+    {
+        if (selectedSlot >= 0 && selectedSlot < inventoryslots.Length)
         {
             inventoryslots[selectedSlot].DeSelect();
         }
-        inventoryslots[newValue].Select();
-        selectedSlot = newValue;
+
+        if (newValue >= 0 && newValue < inventoryslots.Length)
+        {
+            inventoryslots[newValue].Select();
+            selectedSlot = newValue;
+        }
     }
 
+
+    // 아이템을 인벤토리에 추가하는 함수
     public bool AddItem(Item item)
     {
-        for (int i = 0; i < inventoryslots.Length; i++)
+        foreach (InventorySlot slot in inventoryslots)
         {
-            InventorySlot slot = inventoryslots[i];
             InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
             if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < maxItem)
             {
@@ -78,34 +102,92 @@ public class InventoryManager : MonoBehaviour
             }
         }
 
-        for (int i = 0; i < inventoryslots.Length; i++)
+        // 빈 슬롯을 찾아 새로운 아이템을 생성함
+        foreach (InventorySlot slot in inventoryslots)
         {
-            InventorySlot slot = inventoryslots[i];
             InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
             if (itemInSlot == null)
             {
-                SpawNewItem(item, slot);
+                SpawnNewItem(item, slot);
                 return true;
             }
         }
         return false;
     }
 
-    void SpawNewItem(Item item, InventorySlot slot)
+    // 새로운 아이템을 생성하여 지정된 슬롯에 배치하는 함수
+    public InventoryItem SpawnNewItem(Item item, InventorySlot slot, int count = 1)
     {
-        GameObject newItemGet = Instantiate(inventoryItemPrefab, slot.transform);
-        InventoryItem inventoryItem = newItemGet.GetComponent<InventoryItem>();
+        GameObject newItem = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItem.GetComponent<InventoryItem>();
         inventoryItem.InitialiseItem(item);
+        inventoryItem.count = count;
+        inventoryItem.ItemCount();
+        return inventoryItem;
     }
 
+    // 현재 선택된 슬롯의 아이템을 반환하는 함수
     public Item GetSelectedItem()
     {
-        InventorySlot slot = inventoryslots[selectedSlot];
-        InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-        if (itemInSlot != null)
+        if (selectedSlot >= 0 && selectedSlot < inventoryslots.Length)
         {
-            return itemInSlot.item;
+            InventorySlot slot = inventoryslots[selectedSlot];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null) return itemInSlot.item;
         }
         return null;
+    }
+
+    // 드래그한 아이템을 새로운 슬롯으로 이동시키는 함수
+    public void OnItemSwapped(InventoryItem draggedItem, InventorySlot newSlot)
+    {
+        if (draggedItem == null || newSlot == null) return;
+
+        // 새로운 슬롯에 이미 아이템이 있는 경우 처리
+        InventoryItem existingItem = newSlot.GetComponentInChildren<InventoryItem>();
+        if (existingItem != null)
+        {
+            // 같은 아이템일 경우 합침
+            if (existingItem.item == draggedItem.item)
+            {
+                int combinedCount = existingItem.count + draggedItem.count;
+                if (combinedCount <= maxItem)
+                {
+                    existingItem.count = combinedCount;
+                    existingItem.ItemCount();
+                    Destroy(draggedItem.gameObject);
+                }
+                else
+                {
+                    // 합칠 수 있는 만큼 합치고 남은 아이템은 그대로 유지
+                    int remaining = combinedCount - maxItem;
+                    existingItem.count = maxItem;
+                    existingItem.ItemCount();
+                    draggedItem.count = remaining;
+                    draggedItem.ItemCount();
+                    draggedItem.transform.SetParent(draggedItem.parentAfterDrag); // 드래그 시작 전 부모로 돌아감
+                    draggedItem.transform.localPosition = Vector3.zero;
+                }
+            }
+            else
+            {
+                // 다른 아이템일 경우 교환
+                existingItem.transform.SetParent(draggedItem.parentAfterDrag);
+                existingItem.transform.localPosition = Vector3.zero;
+                existingItem.ItemCount();
+
+                // 드래그한 아이템을 새로운 슬롯으로 이동
+                draggedItem.transform.SetParent(newSlot.transform);
+                draggedItem.transform.localPosition = Vector3.zero;
+                draggedItem.parentAfterDrag = newSlot.transform;
+            }
+        }
+        else
+        {
+            // 슬롯이 비어있으면 드래그한 아이템을 이동
+            draggedItem.transform.SetParent(newSlot.transform);
+            draggedItem.transform.localPosition = Vector3.zero;
+            draggedItem.parentAfterDrag = newSlot.transform;
+        }
     }
 }
