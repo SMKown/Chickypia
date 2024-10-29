171fba41d473a4a713db96b07923db1ea7746a7e
HoHyeonMyung <mhohyeon@gmail.com>
2024-10-30 01:03:02 +0900

    A

---
+++ Assets/Scripts/Enemy/Chest.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject Star;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (EnemyAllDead())
+        {
+            animator.SetTrigger("ChestSpawn");
+        }
+    }
+
+    private bool EnemyAllDead()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        return enemies.Length == 0;
+    }
+
+    public void OpenChest()
+    {
+        animator.SetTrigger("ChestOpen");
+    }
+
+    public void SpawnStar()
+    {
+        Star.SetActive(true);
+    }
+}
