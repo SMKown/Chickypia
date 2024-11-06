42f4e1a050344790058c9fd8a7cac0f43eb8bcb5
HoHyeonMyung <mhohyeon@gmail.com>
2024-11-06 16:09:17 +0900

    asdasdsaddsaads

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -139,7 +139,7 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
                 ItemCount();
             }    
         }
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(0.1f);
         PlayerStats.Instance.useItem = false;
     }
 
